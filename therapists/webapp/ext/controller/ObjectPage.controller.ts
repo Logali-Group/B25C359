11d6b467cb92d565ca278374d84b607a25b9a774
Jorge Luis Briceño Amesty <jbriceno@logaligroup.com>
2025-08-16 15:19:13 +0000

    update

--- therapists/webapp/ext/controller/ObjectPage.controller.ts
+++ therapists/webapp/ext/controller/ObjectPage.controller.ts
@@ -4,6 +4,13 @@ import JSONModel from 'sap/ui/model/json/JSONModel';
 import View from 'sap/ui/core/mvc/View';
 import Dialog from 'sap/m/Dialog';
 import Fragment from 'sap/ui/core/Fragment';
+import { DatePicker$ChangeEvent } from 'sap/m/DatePicker';
+import { ComboBox$ChangeEvent } from 'sap/m/ComboBox';
+import ListItem from 'sap/ui/core/ListItem';
+import Context from 'sap/ui/model/odata/v4/Context';
+import SinglePlanningCalendar, { SinglePlanningCalendar$AppointmentDropEvent, SinglePlanningCalendar$AppointmentSelectEvent } from 'sap/m/SinglePlanningCalendar';
+import ODataListBinding from 'sap/ui/model/odata/v4/ODataListBinding';
+import DateFormat from 'sap/ui/core/format/DateFormat';
 
 /**
  * @namespace therapists.ext.controller
@@ -29,6 +36,7 @@ export default class ObjectPage extends ControllerExtension<ExtensionAPI> {
 	private formModel() : void {
 		let data : {
 			patient_ID: string,
+			typeAppointment_ID: string,
 			title: string,
 			description : string,
 			beginDate: Date | null,
@@ -40,6 +48,7 @@ export default class ObjectPage extends ControllerExtension<ExtensionAPI> {
 			endTime: string
 		} = {
 			patient_ID: "",
+			typeAppointment_ID: "",
 			title: "",
 			description :"",
 			beginDate: null,
@@ -78,5 +87,78 @@ export default class ObjectPage extends ControllerExtension<ExtensionAPI> {
 
 	public onClosePress () : void {
 		this.dialog.close();
+	}
+
+	public onChangeDate (event : DatePicker$ChangeEvent) : void {
+		let sDate = event.getParameter("value") as string;
+
+		//@ts-ignore
+		let form = (this.base.getView() as View).getModel("form") as JSONModel;
+
+		form.setProperty("/endDate", sDate);
+		form.setProperty("/startDate", sDate);
+		form.setProperty("/endDate2", sDate);
+	}
+
+	public async onChangeBlock (event: ComboBox$ChangeEvent) : Promise<void> {
+		let item = event.getSource().getSelectedItem() as ListItem;
+		let bindingContext = item.getBindingContext() as Context;
+		let sBeginTime = await bindingContext.requestProperty("beginTime");
+		let sEndTime = await bindingContext.requestProperty("endTime");
+		//@ts-ignore
+		let form = (this.base.getView() as View).getModel("form") as JSONModel;
+		form.setProperty("/beginTime", sBeginTime);
+		form.setProperty("/endTime", sEndTime);
+		form.setProperty("/startDate", form.getProperty("/startDate")+"T"+sBeginTime);
+		form.setProperty("/endDate2", form.getProperty("/endDate")+"T"+sEndTime)
+	}
+
+
+	public async onSavePress () : Promise<void> {
+		//@ts-ignore
+		let form = (this.base.getView() as View).getModel("form") as JSONModel;
+		let body = form.getData();
+		let planningCalendar = this.base.getExtensionAPI().byId("fe::CustomSubSection::PlanningCalendar--calendar") as SinglePlanningCalendar;
+		let bindList = planningCalendar.getBinding("appointments") as ODataListBinding;
+
+		await bindList.create(body).created();
+		this.reset();
+	}
+
+	private reset () : void {
+		this.formModel();
+		this.onClosePress();
+	}
+
+	public async onNavToAppointments (event : SinglePlanningCalendar$AppointmentSelectEvent) : Promise<void> {
+		//@ts-ignore
+		let ui = (this.base.getView() as View).getModel("ui") as JSONModel;
+		let isEditable = ui.getProperty("/isEditable") as boolean;
+		let appointment = event.getParameter("appointment");
+		let context = appointment?.getBindingContext() as Context;
+		let sTherapistID = await context.requestProperty("therapist_ID");
+		let sAppointmentID = await context.requestProperty("ID");
+
+		this.base.getExtensionAPI().getRouting().navigateToRoute("TherapistsSet_toAppointmentsObjectPage", {
+			key: sTherapistID,
+			boolean: !isEditable,
+			toAppointmentsKey: sAppointmentID,
+			boolean2: !isEditable
+		});
+	}
+
+	public async onAppointmentDrop (event : SinglePlanningCalendar$AppointmentDropEvent) : Promise<void> {
+		let oStarDate = event.getParameter("startDate") as Date,
+			sStartDate = DateFormat.getDateInstance({pattern: 'yyyy-MM-dd'}).format(oStarDate),
+			sEndDate = sStartDate,
+			oAppointment = event.getParameter("appointment"),
+			context = oAppointment?.getBindingContext() as Context,
+			sBeginTime = await context.requestProperty("beginTime"),
+			sEndTime = await context.requestProperty("endTime");
+			
+			await context.setProperty("beginDate", sStartDate);
+			await context.setProperty("endDate", sEndDate);
+			await context.setProperty("startDate", sStartDate.concat("T").concat(sBeginTime));
+			await context.setProperty("endDate2", sEndDate.concat("T").concat(sEndTime));
 	}
 }
--- therapists/webapp/ext/fragment/PlanningCalendar.ts
+++ therapists/webapp/ext/fragment/PlanningCalendar.ts
@@ -8,6 +8,30 @@ import MessageToast from 'sap/m/MessageToast';
  * @param this reference to the 'this' that the event handler is bound to.
  * @param event the event object provided by the event provider
  */
-export function onPress(this: ExtensionAPI, event: UI5Event) {
-    MessageToast.show("Custom handler invoked.");
+
+/**
+ * Type01 - Anaranjado
+ * Type02 - Rojo
+ * Type03 - Rojo claro
+ * Type04 - Rojo más claro
+ * Type05 - Violeta
+ * Type06 - Azul
+ * Type07 - Verde claro
+ * Type08 - Verde
+ * Type09 - Gris
+ * Type10 - Morado
+*/
+
+/**
+ * Type02 --> Pending
+ * Type01 --> OnHold
+ * Type08 --> Confirmed
+ */
+
+export function status(this: ExtensionAPI, sStatus: string) {
+    switch (sStatus) {
+        case 'Pending': return 'Type02';
+        case 'OnHold': return 'Type01';
+        case 'Confirmed': return 'Type08';
+    };
 }
